b637dccdeb502d89692722ba9eea9f04f9a049e9
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-06-07 08:21:01 +0000

    namespace導入

---
+++ db/migrate/20240607071645_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id, null: false
+      t.string :genre_id, null: false
+      t.string :title, null: false
+      t.text :body, null: false
+      t.integer :price, null: false
+      t.string :star, null: false
+      t.string :tag, null: false
+      t.timestamps
+    end
+  end
+end
