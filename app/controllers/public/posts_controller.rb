240ef63ec8d85983b45db10d8971fa7d5f2e15e4
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-06-08 09:04:39 +0000

    投稿機能完了

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -12,14 +12,24 @@ class Public::PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to root_path
+    redirect_to post_path(@post.id)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
 
 
   private
 
   def post_params
-    params.require(:post).permit(:image, :title, :body, :genre_id)
+    params.require(:post).permit(:image, :name, :title, :body, :genre_id, :price, :star, :tag)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ApplicationRecord
-belongs_to :user
-has_many :post_comments, dependent: :destroy
-has_many :favorites, dependent: :destroy
-belongs_to :genre
-has_many :tag_relationships, dependent: :destroy
+  belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :tag_relationships, dependent: :destroy
 
   has_one_attached :image
+
+  def get_image
+    unless image.attached?
+      file_path = Ralis.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image
+  end
+
 end
