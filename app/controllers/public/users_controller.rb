b5e4598d9053634ae2c74b548b07d10f67d0c374
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-06-10 02:00:12 +0000

    マイページルーティングエラー

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,3 +1,23 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params.id)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,31 @@
 Rails.application.routes.draw do
+  #管理者用 URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  #管理者側namespaceのルーティング
+
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+
   #顧客用 URL /users/sign_in ...
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-
-  #管理者用 URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-
-
-  root to: 'homes#top'
-  get 'about' => 'homes#about'
+  #顧客側namespaceを含めないルーティング
   scope module: :public do
-    resources :posts, only: [:new, :index, :show, :create, :edit, :update, :destroy]
+    get '/users/my_page' => 'users#show', as: "my_page"
+    get '/users/unsubscribe' => 'users#unsubscribe'
+    patch '/users/withdraw' => 'users#withdraw'
+    get '/searches' => 'searches#search'
+    resources :posts, only: [:new, :index, :create, :edit, :update, :destroy]
+    resources :users, only: [:index, :show, :edit, :update]
+    resource :favotires, only: [:index]
   end
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
