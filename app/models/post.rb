d5b79e144ab28857c4a792fa3e38b1a0c2c8a360
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-06-19 03:22:59 +0000

    休憩

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :genre
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :tag_relationships, dependent: :destroy
