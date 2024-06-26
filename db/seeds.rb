864b678685ea62c7aedb59a2ca6e3c23263a7d27
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-06-26 07:30:29 +0000

    いいね一覧

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,37 @@ Admin.create!(
   password: 'testpass'
 )
 
-Genre.create(id: 1, name: "おもちゃ")
-Genre.create(id: 2, name: "室内遊具")
+
+genres = [
+  {id: 1, name: "おもちゃ"},
+  {id: 2, name: "室内遊具"},
+]
+genres.each do |genre|
+  Genre.find_or_create_by(genre)
+end
+
+tags = [
+  {id: 1, name: "大人"},
+  {id: 2, name: "0ヶ月～"},
+  {id: 3, name: "1ヶ月～"},
+  {id: 4, name: "2ヶ月～"},
+  {id: 5, name: "3ヶ月～"},
+  {id: 6, name: "4ヶ月～"},
+  {id: 7, name: "5ヶ月～"},
+  {id: 8, name: "6ヶ月～"},
+  {id: 9, name: "7ヶ月～"},
+  {id: 10, name: "8ヶ月～"},
+  {id: 11, name: "9ヶ月～"},
+  {id: 12, name: "10ヶ月～"},
+  {id: 13, name: "11ヶ月～"},
+  {id: 14, name: "1歳～"},
+  {id: 15, name: "1歳半～"},
+  {id: 16, name: "2歳～"},
+  {id: 17, name: "3歳～"},
+]
+tags.each do |tag|
+  Tag.find_or_create_by(tag)
+end
 
 kotarou = User.find_or_create_by!(email: "kotarou@example.com") do |user|
   user.last_name = "てすと"
@@ -44,6 +73,7 @@ Post.find_or_create_by!(name: "歯固め") do |post|
   post.body = "お気に入りの歯固めです。"
   post.genre_id = "1"
   post.star = "3"
+  post.tag = "5"
   post.user = kotarou
 end
 
@@ -53,6 +83,7 @@ Post.find_or_create_by!(name: "音が鳴る積み木") do |post|
   post.body = "3種類の音が鳴ります。"
   post.genre_id = "1"
   post.star = "3"
+  post.tag = "12"
   post.user = nijirou
 end
 
@@ -62,5 +93,6 @@ Post.find_or_create_by!(name: "バウンサー") do |post|
   post.body = "バウンサーでゆられているとすぐに寝ちゃいます。"
   post.genre_id = "2"
   post.star = "3"
+  post.tag = "2"
   post.user = gorou
 end
