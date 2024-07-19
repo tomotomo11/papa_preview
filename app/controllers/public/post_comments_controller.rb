043d55d17dafc7bb9175861f0d96b794691458f9
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-07-19 06:36:16 +0000

    コメント非同期側だけ作成

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,10 +1,14 @@
 class Public::PostCommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post), notice: "コメントしました。"
+    @post = Post.find(params[:post_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+      redirect_to post_path(@post), notice: "コメントしました。"
+    else
+      flash[:alert] = "コメントできませんでした。"
+      redirect_to post_path(@post)
+    end
   end
 
   def destroy
